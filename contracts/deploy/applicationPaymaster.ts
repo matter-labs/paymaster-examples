0964ace3b0ed8266a3087f5bf2988ba013fd15f6
Antonio <tonioufa@gmail.com>
2024-12-16 12:48:05 +0000

    feat: app paymaster

---
+++ contracts/deploy/applicationPaymaster.ts
@@ -0,0 +1,65 @@
+import { fundAccount, deployContract } from "./utils";
+import * as hre from "hardhat";
+
+// load env file
+import dotenv from "dotenv";
+dotenv.config();
+
+// load wallet private key from env file
+const PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || "";
+
+if (!PRIVATE_KEY)
+  throw "⛔️ Private key not detected! Add it to the .env file!";
+
+async function main() {
+  const contract = "ApplicationPaymaster";
+  // Application contract address
+  const appContractAddress = "";
+  const artifact = await hre.ethers.loadArtifact(contract);
+
+  console.log(
+    `Running script to deploy ${artifact.contractName} contract on ${hre.network.name}`,
+  );
+
+  // Retrieve signers
+  const [deployer] = await hre.ethers.getSigners();
+
+  // Deploying the paymaster
+  const paymaster = await deployContract(artifact.contractName, [
+    appContractAddress,
+  ]);
+  const paymasterAddress = await paymaster.getAddress();
+  console.log(`Paymaster address: ${paymasterAddress}`);
+  console.log(`Application contract: ${appContractAddress}`);
+
+  console.log("Funding paymaster with ETH");
+  // Supplying paymaster with ETH
+  await fundAccount(deployer, paymasterAddress, "0.005");
+
+  const paymasterBalance = await hre.ethers.provider.getBalance(
+    paymasterAddress,
+  );
+  console.log(`Paymaster ETH balance is now ${paymasterBalance.toString()}`);
+
+  // only verify on testnet and mainnet
+  if (hre.network.name.includes("ZKsyncEra")) {
+    const verificationId = await hre.run("verify:verify", {
+      address: paymasterAddress,
+      // Contract MUST be fully qualified name (e.g. path/sourceName:contractName)
+      contract: `${artifact.sourceName}:${artifact.contractName}`,
+      constructorArguments: [appContractAddress],
+    });
+    console.log(
+      `${artifact.contractName} verified! VerificationId: ${verificationId}`,
+    );
+  }
+
+  console.log(`Done!`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -11,10 +11,7 @@ const TEST_RICH_WALLET =
   "0x3d3cbc973389cb26f657686445bcc75662b415b656078503592ac8c1abb8810e";
 
 const config: HardhatUserConfig = {
-  zksolc: {
-    version: "latest",
-    settings: {},
-  },
+  zksolc: {},
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
